09f347676bb5db59aa9c7183b20e6e2c4764bffc
Rubens Castro <rubenscauanfc2021@gmail.com>
2026-01-16 14:56:38 -0300

    [Rubens] - Ajuste da porta liberada pelo cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import { createRouter } from "./routes";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5176",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
